cfd4c4c471910ef0e1cdb156b5c6c7fae72a2779
DanielFloria <daniflorialopez@gmail.com>
2022-09-15 09:52:18 +0200

    Primer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AltaComponent } from './alta/alta.component';
+import { ListadoComponent } from './listado/listado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AltaComponent,
+    ListadoComponent
   ],
   imports: [
     BrowserModule,
